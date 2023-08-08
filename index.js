01e2841324ae0e62dd86fe4acced58d1c9dee74a
Henrique Godez <henriquegodez@gmail.com>
2023-08-07 21:51:10 -0300

    organize code

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 
-let arena = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
-]
+let arena = []
 
 // CONSTANTS
 const CANVASHEIGHT = canvas.height
 const CANVASWIDTH = canvas.width
-const PIXEL = CANVASWIDTH / arena[0].length
+const ARENA_X_LENGTH = 10
+const ARENA_Y_LENGTH = ARENA_X_LENGTH * 2
+const PIXEL = CANVASWIDTH / ARENA_X_LENGTH
 const LEFT_ARROW = 37
 const RIGHT_ARROW = 39
 const DOWN_ARROW = 40
@@ -246,6 +227,14 @@ const O_KICK_OFFSET_DATA = {
   1: [{x: 0, y: 1}],
   2: [{x: -1, y: 1}],
   3: [{x: -1, y: 0}]
+}
+
+const createEmptyArena = (width, height) => 
+{ const matrix = [] 
+  while (height--) { 
+    matrix.push(new Array(width).fill(0)) 
+  } 
+  return matrix 
 }
 
 const createPiece = (posX, posY, tetromino, rotation = 0, kickOffsetData = JLSTZ_KICK_OFFSET_DATA) => {
@@ -518,7 +507,7 @@ const draw = () => {
   insertPixelDataIntoArena()
   insertPieceIntoArena(player.piece)
   drawArenaData()
-  //printArenaData()
+  printArenaData()
 }
 
 let lastime = 0
@@ -532,214 +521,11 @@ const update = (time = 0) => {
   requestAnimationFrame(update)
 }
 
-update()
-
-
-// ARENA PRESETS
-
-const createElevator = () => {
-  player.piece = createStartingPiece('T')
-  pixelData = [
-    createPixelData(0,1,1),
-    createPixelData(0,2,1),
-    createPixelData(0,3,1),
-    createPixelData(0,4,1),
-    createPixelData(0,5,1),
-    createPixelData(0,6,1),
-    createPixelData(0,7,1),
-    createPixelData(0,8,1),
-    createPixelData(0,9,1),
-    createPixelData(0,10,1),
-    createPixelData(0,11,1),
-    createPixelData(0,12,1),
-    createPixelData(0,13,1),
-    createPixelData(0,14,1),
-    createPixelData(0,15,1),
-    createPixelData(0,16,1),
-    createPixelData(0,17,1),
-    createPixelData(0,18,1),
-    createPixelData(0,19,1),
-  
-    createPixelData(1,5,1),
-    createPixelData(1,6,1),
-    createPixelData(1,7,1),
-    createPixelData(1,8,1),
-    createPixelData(1,9,1),
-    createPixelData(1,10,1),
-    createPixelData(1,11,1),
-    createPixelData(1,12,1),
-    createPixelData(1,13,1),
-    createPixelData(1,14,1),
-    createPixelData(1,15,1),
-    createPixelData(1,16,1),
-    createPixelData(1,17,1),
-    createPixelData(1,18,1),
-    createPixelData(1,19,1),
-  
-    createPixelData(2,9,1),
-    createPixelData(2,10,1),
-    createPixelData(2,11,1),
-    createPixelData(2,12,1),
-    createPixelData(2,13,1),
-    createPixelData(2,14,1),
-    createPixelData(2,15,1),
-    createPixelData(2,16,1),
-    createPixelData(2,17,1),
-    createPixelData(2,18,1),
-    createPixelData(2,19,1),
-  
-    createPixelData(3,13,1),
-    createPixelData(3,14,1),
-    createPixelData(3,15,1),
-    createPixelData(3,16,1),
-    createPixelData(3,17,1),
-    createPixelData(3,18,1),
-    createPixelData(3,19,1),
-  
-    createPixelData(4,17,1),
-    createPixelData(4,18,1),
-    createPixelData(4,19,1),
-  
-    createPixelData(2,3,1),
-    createPixelData(3,7,1),
-    createPixelData(4,11,1),
-    createPixelData(5,15,1),
-  
-    createPixelData(6,19,1),
-    createPixelData(7,19,1),
-    createPixelData(8,19,1),
-    createPixelData(9,19,1),
-  
-  
-    createPixelData(4,2,1),
-    createPixelData(4,3,1),
-    createPixelData(4,4,1),
-  
-    createPixelData(5,2,1),
-    createPixelData(5,3,1),
-    createPixelData(5,4,1),
-    createPixelData(5,5,1),
-    createPixelData(5,6,1),
-    createPixelData(5,7,1),
-    createPixelData(5,8,1),
-  
-    createPixelData(6,2,1),
-    createPixelData(6,3,1),
-    createPixelData(6,4,1),
-    createPixelData(6,5,1),
-    createPixelData(6,6,1),
-    createPixelData(6,7,1),
-    createPixelData(6,8,1),
-    createPixelData(6,9,1),
-    createPixelData(6,10,1),
-    createPixelData(6,11,1),
-    createPixelData(6,12,1),
-  
-    createPixelData(7,2,1),
-    createPixelData(7,3,1),
-    createPixelData(7,4,1),
-    createPixelData(7,5,1),
-    createPixelData(7,6,1),
-    createPixelData(7,7,1),
-    createPixelData(7,8,1),
-    createPixelData(7,9,1),
-    createPixelData(7,10,1),
-    createPixelData(7,11,1),
-    createPixelData(7,12,1),
-    createPixelData(7,13,1),
-    createPixelData(7,14,1),
-    createPixelData(7,15,1),
-    createPixelData(7,16,1),]
+const start = () => {
+  arena = createEmptyArena(ARENA_X_LENGTH, ARENA_Y_LENGTH)
+  //elevatorArenaPreset(player, pixelData)
+  //stairsArenaPreset(player, pixelData)
+  update()
 }
 
-const createStairs = () => {
-  player.piece = createStartingPiece('I')
-  pixelData = [
-    createPixelData(0,11,5),
-    createPixelData(1,11,5),
-    createPixelData(2,11,5),
-    createPixelData(3,11,5),
-    createPixelData(4,11,5),
-    createPixelData(5,11,5),
-    createPixelData(6,11,5),
-    createPixelData(7,11,5),
-    createPixelData(9,11,5),
-
-    createPixelData(0,12,5),
-    createPixelData(1,12,5),
-    createPixelData(2,12,5),
-    createPixelData(3,12,5),
-    createPixelData(4,12,5),
-    createPixelData(5,12,5),
-    createPixelData(6,12,5),
-    createPixelData(7,12,5),
-    createPixelData(9,12,5),
-
-    createPixelData(0,13,5),
-    createPixelData(1,13,5),
-    createPixelData(2,13,5),
-    createPixelData(3,13,5),
-    createPixelData(4,13,5),
-    createPixelData(5,13,5),
-    createPixelData(6,13,5),
-    createPixelData(7,13,5),
-    createPixelData(9,13,5),
-
-    createPixelData(0,14,5),
-    createPixelData(1,14,5),
-    createPixelData(2,14,5),
-    createPixelData(3,14,5),
-    createPixelData(4,14,5),
-    createPixelData(5,14,5),
-    createPixelData(6,14,5),
-    createPixelData(7,14,5),
-    createPixelData(9,14,5),
-
-    createPixelData(0,15,5),
-    createPixelData(1,15,5),
-    createPixelData(2,15,5),
-    createPixelData(3,15,5),
-    createPixelData(9,15,5),
-
-    createPixelData(0,16,5),
-    createPixelData(1,16,5),
-    createPixelData(2,16,5),
-    createPixelData(3,16,5),
-    createPixelData(5,16,5),
-    createPixelData(6,16,5),
-    createPixelData(7,16,5),
-    createPixelData(8,16,5),
-    createPixelData(9,16,5),
-
-    createPixelData(0,17,5),
-    createPixelData(1,17,5),
-    createPixelData(2,17,5),
-    createPixelData(3,17,5),
-    createPixelData(5,17,5),
-    createPixelData(6,17,5),
-    createPixelData(7,17,5),
-    createPixelData(8,17,5),
-    createPixelData(9,17,5),
-
-    createPixelData(0,18,5),
-    createPixelData(1,18,5),
-    createPixelData(2,18,5),
-    createPixelData(3,18,5),
-    createPixelData(5,18,5),
-    createPixelData(6,18,5),
-    createPixelData(7,18,5),
-    createPixelData(8,18,5),
-    createPixelData(9,18,5),
-
-    createPixelData(0,19,5),
-    createPixelData(5,19,5),
-    createPixelData(6,19,5),
-    createPixelData(7,19,5),
-    createPixelData(8,19,5),
-    createPixelData(9,19,5),
-    
-  ]
-}
-
-//createElevator()
-//createStairs()
+start()
